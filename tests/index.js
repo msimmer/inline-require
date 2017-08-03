128e1652668f7a76de75c25f0216c610ebbe92fa
Maxwell Simmer <maxwell.simmer@gmail.com>
2017-08-03 22:01:15 +0200

    Update .gitignore

---
+++ tests/index.js
@@ -0,0 +1,13 @@
+const array = require('./deps/array')
+const object = require('./deps/object')
+const number = require('./deps/number')
+const string = require('./deps/string')
+const func = require('./deps/func')
+
+module.exports = {
+    array,
+    object,
+    number,
+    string,
+    func,
+}
